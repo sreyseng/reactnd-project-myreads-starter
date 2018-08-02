8e19891ad2f01422041beee1f3bd27b6aa98904e
Srey Seng <sreyseng@gmail.com>
2018-08-02 16:34:46 -0700

    Implement search

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './../BooksAPI'
+
+
+import BookListItem from './book_list_item';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: '',
+      results: []
+    }
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.bookSearch(term);
+  }
+
+  bookSearch(term) {
+    if(term && term.length > 0) {
+        BooksAPI.search(term).then((results) => {
+        if (results && results.length > 0) {
+          this.setState({results})
+        }
+        }).catch(err => {
+          console.log(err)
+        })
+    }
+  }
+
   render() {
     return (
       <div className="search-books">
@@ -10,11 +40,17 @@ class Search extends Component {
           Close
         </Link>
         <div className="search-books-input-wrapper">
-          <input type="text" placeholder="Search by title or author"/>
+          <input
+            type="text"
+            placeholder="Search by title or author"
+            value={this.state.term}
+            onChange={(event) => this.onInputChange(event.target.value)} />
         </div>
       </div>
       <div className="search-books-results">
-        <ol className="books-grid"></ol>
+        <ol className="books-grid">
+          {this.state.results.map((book) => <BookListItem key={book.id} book={book} />)}
+        </ol>
       </div>
     </div>
     );
