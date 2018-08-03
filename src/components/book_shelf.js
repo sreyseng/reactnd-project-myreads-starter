72550cf14b2eb6118b449fc2ac904c2fb9eec89b
Srey Seng <sreyseng@gmail.com>
2018-08-02 19:52:00 -0700

    Refactor: change Bookshelf to stateless component

--- src/components/book_shelf.js
+++ src/components/book_shelf.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import BookListItem from './book_list_item';
 
-class Bookshelf extends Component {
-  render() {
-    const { shelf, books, onShelfChange } = this.props;
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{shelf.value}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map((book) => (
-              <BookListItem
-                key={book.id}
-                book={book}
-                shelfKey={shelf.key}
-                onShelfChange={onShelfChange}
-              />
-            ))}
-          </ol>
-        </div>
+const Bookshelf = ({ shelf, books, onShelfChange }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{shelf.value}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <BookListItem
+              key={book.id}
+              book={book}
+              shelfKey={shelf.key}
+              onShelfChange={onShelfChange}
+            />
+          ))}
+        </ol>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Bookshelf;
